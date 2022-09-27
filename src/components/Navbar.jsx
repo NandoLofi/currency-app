03db06bbd26249c1de4828cc0b62f313023c4c64
Fernando Galvan <n.brighter00@gmail.com>
2022-09-26 21:08:29 -0500

    basic component setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuFoldOutlined } from '@ant-design/icons'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='nav__container'>
+        <div className="logo__container">
+            <Avatar />
+            <Typography.Title level={2} className="logo"/>
+        </div>
+    </div>
   )
 }
