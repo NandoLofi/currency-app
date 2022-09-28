2198933c4b48fc470c12d88884633bc6c6b1df67
Fernando Galvan <n.brighter00@gmail.com>
2022-09-27 21:19:54 -0500

    adding component layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuFoldOutlined } from '@ant-design/icons'
-
+import icon from '../images/Cry'
 export default function Navbar() {
   return (
     <div className='nav-container'>
         <div className="logo-container">
-            <Avatar />
+            <Avatar src={icon} size="large"/>
             <Typography.Title level={2} className="logo">
               <Link to="/">Home</Link>
             </Typography.Title>
