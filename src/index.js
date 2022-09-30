62da5c41e90e74be3f5ca3756fd51676108c1e67
Fernando Galvan <n.brighter00@gmail.com>
2022-09-29 20:44:04 -0500

    added store and reducers with services folder added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 import App from './App'
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import 'antd/dist/antd.min.css'
+import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+    <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+    </Provider>
   </Router>
 );
---
+++ src/services/currencyApi.js
@@ -0,0 +1,24 @@
+import create from '@ant-design/icons/lib/components/IconFont'
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const apiHeaders = {
+    'X-RapidAPI-Key': '6c087f1dafmshee4c79cbea1744cp1e4819jsncbb07ebc3b6e',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+
+}
+
+const BASE_URL = 'https://coinranking1.p.rapidapi.com/coins'
+
+const createRequest = (url) => ({ url, headers: apiHeaders })
+
+const currencyApi = createApi({
+    reducerPath: 'currencyApi',
+    baseQuery: fetchBaseQuery({
+        BASE_URL
+    }),
+    endpoints: (builder) => ({
+        getCurrency: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
