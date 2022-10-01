5c2a4a0c9c989b91b6c92c1d8104062dcbc9cf90
Fernando Galvan <n.brighter00@gmail.com>
2022-10-01 16:30:02 -0500

    api call giving error

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { currencyApi } from "../services/currencyApi";
+import { currencyApi } from "../services/currencyApi"
+
 
 export default configureStore ({
     reducer: {
-        [currencyApi.reducerPath]: currencyApi.reducer,
+        [ currencyApi.reducerPath ]: currencyApi.reducer,
     },
 })
--- src/services/currencyApi.js
+++ src/services/currencyApi.js
@@ -1,28 +1,42 @@
-import create from '@ant-design/icons/lib/components/IconFont'
-import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import axios from "axios";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 
 const apiHeaders = {
-    'X-RapidAPI-Key': '6c087f1dafmshee4c79cbea1744cp1e4819jsncbb07ebc3b6e',
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-
 }
+const createRequest = (url)=>({url, headers: apiHeaders})
 
-const BASE_URL = 'https://coinranking1.p.rapidapi.com/coins'
-
-const createRequest = (url) => ({ url, headers: apiHeaders })
+const BASE_URL = "https://coinranking1.p.rapidapi.com/coins"
 
 export const currencyApi = createApi({
     reducerPath: 'currencyApi',
-    baseQuery: fetchBaseQuery({
-        BASE_URL
-    }),
+    baseQuery: fetchBaseQuery({ BASE_URL }),
     endpoints: (builder) => ({
         getCurrency: builder.query({
-        query: () => createRequest('/coins')
+            query: ()=> createRequest('/coins')
         })
     })
 })
 
 export const {
-    useGetCurrencyQuery
+    useGetCurrencyQuery,
 } = currencyApi
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+
+//     }
+//   };
