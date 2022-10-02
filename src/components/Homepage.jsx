331d9b9503756bd092ea0ed2cad125a329fc229d
Fernando Galvan <n.brighter00@gmail.com>
2022-10-02 16:56:56 -0500

    added display and having home page display info

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,17 +8,27 @@ const { Title } = Typography
 
 export default function Homepage() {
   const { data, isFetching } = useGetCurrencyQuery();
+  const globalStats = data ?.data ?.stats
+
+
+
   console.log(data)
+if(isFetching) return 'Loading...'
+
   return (
     <>
         <Title leve={2} className="heading">Global Currency Stats </Title>
         <Row>
-            <Col span={12}><Statistic title="Total Currency" value="5"/></Col>
-            <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-            <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-            <Col span={12}><Statistic title="Total 24 Hour Volume" value="5"/></Col>
-            <Col span={12}><Statistic title="Total Market" value="5"/></Col>
+            <Col span={12}><Statistic title="Total Currency" value={globalStats.total}/></Col>
+            <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+            <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+            <Col span={12}><Statistic title="Total 24 Hour Volume" value={millify(globalStats.total24hVolume)} /></Col>
+            <Col span={12}><Statistic title="Total Market" value={millify(globalStats.totalMarkets)} /></Col>
         </Row>
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">Top 10 Currencies in the World</Title>
+          <Title level={3} className="show-more"><Link to="/currencies">Show</Link></Title>
+        </div>
     </>
   )
 }
