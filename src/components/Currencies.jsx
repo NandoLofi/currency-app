6df708de8f6f524ef37e5f5341933dfbae2865f3
Fernando Galvan <n.brighter00@gmail.com>
2022-10-04 12:45:56 -0500

    added search bar and cleaned up code with help

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -7,12 +7,12 @@ import { useState, useEffect } from 'react'
 import { useGetCurrencyQuery } from '../services/currencyApi'
 
 export default function Currencies({ simplified }) {
-  const count = simplified ? 10 : 100;
+  const count = simplified?10 : 100;
   const { data: currencyList, isFetching } = useGetCurrencyQuery(count)
   const [ currency, setCurrency ] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   useEffect(()=>{
-    const filteredData = currencyList ?.data ?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    const filteredData = currencyList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
     setCurrency(filteredData)
 
@@ -25,14 +25,17 @@ export default function Currencies({ simplified }) {
   
   return (
     <>
+    { (!simplified && 
     <div className="search-crypto">
       <Input placeholder='Search' onChange={(e)=> setSearchTerm(e.target.value)}/>
     </div>
+    )}
+
       <Row gutter={[32, 32]} className='crypto-card-container'>
-        {currency ?.map((currency)=> (
+        {currency?.map((currency)=> (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/currencies/${currency.id}`}>
-              <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl}/>} hoverable>
+              <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl} alt={currency.name} />} hoverable>
                 <p> Price: {millify(currency.price)}</p>
                 <p>MarketCap: {millify(currency.marketCap)}</p>
                 <p>DailyChange: {millify(currency.change)}%</p>
