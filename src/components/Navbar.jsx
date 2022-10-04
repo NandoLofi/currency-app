07a9fcf8f70c143ea6c3b6093ad18f75f2484341
Fernando Galvan <n.brighter00@gmail.com>
2022-10-04 13:14:26 -0500

    added some different styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ export default function Navbar() {
   return (
     <div className='nav-container'>
         <div className="logo-container">
-            <Avatar src={icon} size="small"/>
+            <Avatar src={icon} size="large"/>
             <Typography.Title level={2} className="logo">
               <Link to="/">Home</Link>
             </Typography.Title>
         </div>
         <Menu theme='dark'>
           <Menu.Item icon={<HomeOutlined/>}>
-            <Link to="/"> </Link>
+            <Link to="/">Home</Link>
           </Menu.Item>
           <Menu.Item icon={<FundOutlined/>}>
             <Link to="/currencies"> Currencies </Link>
